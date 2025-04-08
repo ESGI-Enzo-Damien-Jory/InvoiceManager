6363ad526fce961e5d17c5f9b721063e7ec18d82
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-08 17:57:52 +0200

    chore(Project): Migrate to turbo

--- run.ps1
+++
@@ -1,9 +0,0 @@
-# Set the paths for the frontend and backend directories
-$frontendPath = "frontend"
-$backendPath = "backend"
-
-# Start frontend in a new PowerShell window
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd `"$frontendPath`"; npm run dev"
-
-# Start backend in another new PowerShell window
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd `"$backendPath`"; npm run start"
