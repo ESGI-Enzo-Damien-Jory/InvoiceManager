362db7796541683c0df0d2f7bc41b09d0f5587b3
Nithard Damien <damien.nithard@gmail.com>
2025-05-16 23:05:22 +0200

    feat(clients): Add show method to fetch a specific client by ID

--- apps/backend/start/routes.ts
+++ apps/backend/start/routes.ts
@@ -37,6 +37,7 @@ router
 router
   .group(() => {
     router.get('/', [ClientsController, 'index'])
+    router.get('/:id', [ClientsController, 'show'])
     router.post('/', [ClientsController, 'store'])
     router.put('/:id', [ClientsController, 'update'])
     router.delete('/:id', [ClientsController, 'destroy'])
