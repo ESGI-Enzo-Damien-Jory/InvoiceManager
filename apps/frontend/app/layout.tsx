a2f479ecb0be47a818f07a3951f91e2c7869730f
Enzo Hugonnier <enzohugonn@gmail.com>
2025-05-22 11:47:54 +0200

    fix(Lint): Fix lint issues

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/custom/app-sidebar'
