3529edeff41ed76a8d528fb9d0fb1b88ae097651
Enzo Hugonnier <enzohugonn@gmail.com>
2025-05-31 20:12:00 +0200

    refactor(Layout): Use sidebar provider

--- apps/frontend/app/(protected)/layout.tsx
+++ apps/frontend/app/(protected)/layout.tsx
@@ -1,13 +1,27 @@
-import { SidebarProvider } from '@/components/ui/sidebar'
-import { AppSidebar } from '@/components/custom/app-sidebar'
+import { AppSidebar } from '@/components/app-sidebar'
+import { SiteHeader } from '@/components/site-header'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import './theme.css'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen">
-            <SidebarProvider defaultOpen={true}>
-                <AppSidebar />
-                {children}
-            </SidebarProvider>
-        </div>
+        <SidebarProvider
+            style={
+                {
+                    '--sidebar-width': 'calc(var(--spacing) * 72)',
+                    '--header-height': 'calc(var(--spacing) * 12)',
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+                <SiteHeader />
+                <div className="flex flex-1 flex-col">
+                    <div className="@container/main flex flex-1 flex-col gap-2">
+                        {children}
+                    </div>
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
