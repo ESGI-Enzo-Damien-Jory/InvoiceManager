de4b61dc15a854493d9d5db710f6f282e7fbaac7
Enzo Hugonnier <enzohugonn@gmail.com>
2025-05-31 22:48:02 +0200

    feat(Layout) Setup default align center on layout

--- apps/frontend/app/(protected)/layout.tsx
+++ apps/frontend/app/(protected)/layout.tsx
@@ -17,7 +17,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <SidebarInset>
                 <SiteHeader />
                 <div className="flex flex-1 flex-col">
-                    <div className="@container/main flex flex-1 flex-col gap-2">
+                    <div className="@container/main flex flex-1 flex-col gap-2 justify-center">
                         {children}
                     </div>
                 </div>
