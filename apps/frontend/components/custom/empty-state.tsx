00d01c1ad2402d653680e72927027386bcce3276
Enzo Hugonnier <enzohugonn@gmail.com>
2025-06-01 22:31:54 +0200

    chore(EmptyState): Refacto to accept onLinkClick

--- apps/frontend/components/custom/empty-state.tsx
+++ apps/frontend/components/custom/empty-state.tsx
@@ -8,7 +8,8 @@ interface EmptyStateProps {
     title?: string
     description: string
     linkText: string
-    linkHref: string
+    linkHref?: string
+    onLinkClick?: () => void
     icon?: ReactNode
 }
 
@@ -17,35 +18,40 @@ const EmptyState: FC<EmptyStateProps> = ({
     description,
     linkText,
     linkHref,
+    onLinkClick,
     icon,
 }) => (
-    <div className="flex items-center justify-center w-full">
-        <div className="flex flex-col items-center space-y-4">
-            {icon ?? (
-                // Icône par défaut si aucune n’est passée
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-12 w-12 text-muted-foreground"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8m-18 8h18"
-                    />
-                </svg>
-            )}
-            <h2 className="text-xl font-semibold">{title}</h2>
-            <p className="text-center text-sm text-muted-foreground">
-                {description}
-            </p>
-            <Link href={linkHref}>
+    <div className="flex flex-col items-center justify-center py-16 space-y-4">
+        {icon ?? (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-12 w-12 text-muted-foreground"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8m-18 8h18"
+                />
+            </svg>
+        )}
+        <h2 className="text-xl font-semibold">{title}</h2>
+        <p className="max-w-xs text-center text-sm text-muted-foreground">
+            {description}
+        </p>
+
+        {onLinkClick ? (
+            <Button variant="outline" onClick={onLinkClick}>
+                {linkText}
+            </Button>
+        ) : (
+            <Link href={linkHref || '#'}>
                 <Button variant="outline">{linkText}</Button>
             </Link>
-        </div>
+        )}
     </div>
 )
 
