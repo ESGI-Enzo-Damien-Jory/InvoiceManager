0c8e1c19b8be8513963b668641dc0e6bde14383d
Enzo Hugonnier <enzohugonn@gmail.com>
2025-06-17 19:59:20 +0200

    chore(Frontend): Remove previous trypes

--- apps/frontend/types/items.ts
+++
@@ -1,5 +0,0 @@
-import type { Database } from './database'
-
-export type Item = Database['public']['Tables']['items']['Row']
-export type NewItem = Database['public']['Tables']['items']['Insert']
-export type UpdateItem = Database['public']['Tables']['items']['Update']
--- apps/frontend/types/users.ts
+++
@@ -1,5 +0,0 @@
-import type { Database } from './database'
-
-export type User = Database['public']['Tables']['users']['Row']
-export type NewUser = Database['public']['Tables']['users']['Insert']
-export type UpdateUser = Database['public']['Tables']['users']['Update']
